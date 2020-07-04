21b0c8a3d759be654df184bea0dc5623b62d26c2
andrewfromtver <59865403+andrewfromtver@users.noreply.github.com>
2020-07-04 05:59:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ function searchList() {
         <h3 class="title">
             По вашему запросу ничего не найденно
         </h3>
-        <img class="img" src="./img/not_found.gif" alt="69store__404">
+        <img class="img" src="./img/404.gif" alt="69store__404">
         <button class="btn" onclick="mainList()">На главную страницу</button>
     `;
     }
